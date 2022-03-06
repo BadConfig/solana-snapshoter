04f9b857513281b4674cbf2795ec3935a6031852
badconfig <array.clean@gmail.com>
2022-03-06 19:37:56 +0300

    async

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
+use futures::future;
 use serde_json;
 use std::fs;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Error, Write};
-use serde;
+use tokio_stream::{self as stream, StreamExt};
+use std::sync::Arc;
 
-#[tokio::main]
-async fn main() {
-    let path = "./src/results.json";
-    let data = fs::read_to_string(path).expect("Unable to read file");
-    let input: HashMap<String,String> = serde_json::from_str(&data).unwrap();
-
+pub async fn process(input: Vec<(String,String)>,mut count: usize) -> Vec<(String,String)> {
+    let mut res = Vec::with_capacity(input.len());
     let client = reqwest::Client::new();
-
-    let mut op_creator = HashMap::<String,String>::new();
-    let mut op_pda = HashMap::<String,String>::new();
-    let mut count = 0;
-
-    for (k,v) in input.into_iter() {
+    for (k,v) in input {
         let request_data = serde_json::json!({
             "jsonrpc": "2.0",
             "id": 1u64,
@@ -73,18 +66,47 @@ async fn main() {
 
         count += 1;
         println!("[{}/10000] address {} minter {}",count,k,owner);
-        op_creator.insert(k.clone(),owner);
-        op_pda.insert(k,asociated.to_string());
+        //op_creator.insert(k.clone(),owner);
+        //op_pda.insert(k,asociated.to_string());
+        res.push((k,owner))
     }
-    let path = "./src/metadata_to_creators.json";
+    res
+}
+
+#[tokio::main]
+async fn main() {
+    let path = "./src/results.json";
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let input: HashMap<String,String> = serde_json::from_str(&data).unwrap();
+    let input = input.into_iter().collect::<Vec<(String,String)>>();
+
+
+    let mut op_creator = HashMap::<String,String>::new();
+    let mut op_pda = HashMap::<String,String>::new();
+    let mut handles = Vec::new();
+    for i in 0..100 {
+        let inp = input[(i*100)..((i+1)*100)].to_owned();
+        handles.push(tokio::spawn(async move {
+            process(inp, i * 100).await
+        }));
+    }
+
+    let mut res = Vec::new();
+    for i in handles {
+        res.append(&mut i.await.unwrap());
+    }
+
+    op_creator = res.into_iter().collect::<HashMap<String,String>>();
+
+    let path = "./src/metadata_to_creators_.json";
     let mut output = File::create(path).unwrap();
     let srt = serde_json::Value::from_iter(op_creator.into_iter());
     write!(output,"{}",serde_json::to_string(&srt).unwrap()).unwrap();
 
-    let path = "./src/metadata_to_wallets.json";
-    let mut output = File::create(path).unwrap();
-    let srt = serde_json::Value::from_iter(op_pda.into_iter());
-    write!(output,"{}",serde_json::to_string(&srt).unwrap()).unwrap();
+    //let path = "./src/metadata_to_wallets_2000_3000.json";
+    //let mut output = File::create(path).unwrap();
+    //let srt = serde_json::Value::from_iter(op_pda.into_iter());
+    //write!(output,"{}",serde_json::to_string(&srt).unwrap()).unwrap();
 
     println!("finished!");
 }
